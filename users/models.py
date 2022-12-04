e4bc3b3d3683dff14a77eb79c218567c0b782bc8
FrozZzy1 <frozzzy71@gmail.com>
2022-12-05 03:34:24 +0700

    create User

---
+++ users/models.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        if not username:
+            raise TypeError('Users must have a username.')
+
+        if not email:
+            raise TypeError('Users must have a email address.')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, email, password):
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField('Имя пользователя', max_length=255)
+    email = models.EmailField('Почта пользователя', unique=True, db_index=True)
+    avatar = models.ImageField(verbose_name='Аватарка пользователя', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
