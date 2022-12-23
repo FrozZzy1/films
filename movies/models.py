cdad5e274b9f8b25efd65080397e795dede11629
FrozZzy1 <frozzzy71@gmail.com>
2022-12-23 22:33:12 +0700

    add directors m2m add scripts m2m add producers m2m add composers m2m add artists m2m

--- movies/models.py
+++ movies/models.py
@@ -23,6 +23,61 @@ class Country(models.Model):
         verbose_name_plural = 'Countries'
 
 
+class Director(models.Model):
+    title = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Director'
+        verbose_name_plural = 'Directors'
+
+
+class Script(models.Model):
+    title = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Script'
+        verbose_name_plural = 'Scripts'
+
+
+class Producer(models.Model):
+    title = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Producer'
+        verbose_name_plural = 'Producers'
+
+
+class Composer(models.Model):
+    title = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Composer'
+        verbose_name_plural = 'Composers'
+
+
+class Artist(models.Model):
+    title = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Artist'
+        verbose_name_plural = 'Artists'
+
+
 class Movie(models.Model):
     LIST_OF_RATING_MPAA = (
         ('G', 'G'),
@@ -38,6 +93,11 @@ class Movie(models.Model):
     year = models.PositiveIntegerField()
     countries = models.ManyToManyField(Country)
     genres = models.ManyToManyField(Genre)
+    directors = models.ManyToManyField(Director)
+    scripts = models.ManyToManyField(Script)
+    producers = models.ManyToManyField(Producer)
+    composers = models.ManyToManyField(Composer)
+    artists = models.ManyToManyField(Artist)
     fees_us = models.PositiveIntegerField(default=0)
     fees_world = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='media', blank=True)
--- movies/serializers.py
+++ movies/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from movies.models import Movie, Genre, Country
+from movies.models import Movie, Genre, Country, Script, Director, Producer, Composer, Artist
 
 
 class GenreSerializer(serializers.ModelSerializer):
@@ -19,9 +19,54 @@ class CountrySerializer(serializers.ModelSerializer):
                   ]
 
 
+class DirectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Director
+        fields = ['id',
+                  'title',
+                  ]
+
+
+class ScriptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Script
+        fields = ['id',
+                  'title',
+                  ]
+
+
+class ProducerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Producer
+        fields = ['id',
+                  'title',
+                  ]
+
+
+class ComposerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Composer
+        fields = ['id',
+                  'title',
+                  ]
+
+
+class ArtistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Artist
+        fields = ['id',
+                  'title',
+                  ]
+
+
 class MovieSerializer(serializers.ModelSerializer):
     genres = GenreSerializer(many=True)
     countries = CountrySerializer(many=True)
+    directors = DirectorSerializer(many=True)
+    scripts = ScriptSerializer(many=True)
+    producers = ProducerSerializer(many=True)
+    composers = ComposerSerializer(many=True)
+    artists = ArtistSerializer(many=True)
 
     class Meta:
         model = Movie
@@ -32,6 +77,11 @@ class MovieSerializer(serializers.ModelSerializer):
                   'year',
                   'countries',
                   'genres',
+                  'directors',
+                  'scripts',
+                  'producers',
+                  'composers',
+                  'artists',
                   'fees_us',
                   'fees_world',
                   'image',
@@ -41,6 +91,14 @@ class MovieSerializer(serializers.ModelSerializer):
 
 
 class MovieDetailSerializer(serializers.ModelSerializer):
+    genres = GenreSerializer(many=True)
+    countries = CountrySerializer(many=True)
+    directors = DirectorSerializer(many=True)
+    scripts = ScriptSerializer(many=True)
+    producers = ProducerSerializer(many=True)
+    composers = ComposerSerializer(many=True)
+    artists = ArtistSerializer(many=True)
+
     class Meta:
         model = Movie
         fields = ['id',
@@ -50,6 +108,11 @@ class MovieDetailSerializer(serializers.ModelSerializer):
                   'year',
                   'countries',
                   'genres',
+                  'directors',
+                  'scripts',
+                  'producers',
+                  'composers',
+                  'artists',
                   'fees_us',
                   'fees_world',
                   'image',
