bd50f38717f9c7a43b6edd9e0a4e3328c9641e89
FrozZzy1 <frozzzy71@gmail.com>
2023-10-18 01:16:41 +0700

    add(db): add `usermodel` to db

---
+++ db/users.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+
+from db.abstract import AbstractModel
+
+
+class UserModel(AbstractModel):
+    username = Column(String, unique=True)
+    password = Column(String)
+    first_name = Column(String)
+    middle_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow)
