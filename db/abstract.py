ff74c11038acc109ffbe696de8edd4a834f263ba
FrozZzy1 <frozzzy71@gmail.com>
2023-10-19 01:57:27 +0700

    refactor(db): delete automatic `__tablename__` creation

--- db/abstract.py
+++ db/abstract.py
@@ -4,9 +4,6 @@ from sqlalchemy.orm import as_declarative, declared_attr
 
 @as_declarative
 class AbstractModel:
-    id = Column(Integer, autoincrement=True, primary_key=True)
+    __tablename__ = 'abstactmodel'
 
-    @classmethod
-    @declared_attr
-    def __tablename__(cls):
-        return cls.__name__.lower()
+    id = Column(Integer, autoincrement=True, primary_key=True)
