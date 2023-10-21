3f770d3a3d67cb228128ea70e7a96cb78d003771
FrozZzy1 <frozzzy71@gmail.com>
2023-10-21 18:44:29 +0700

    add(db): add comment creator

--- db/comments.py
+++ db/comments.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 
 from db.abstract import AbstractModel
 
@@ -7,6 +7,7 @@ from db.abstract import AbstractModel
 class CommentModel(AbstractModel):
     __tablename__ = 'comments'
 
+    creator = Column(Integer, ForeignKey('users.id'), nullable=False)
     text = Column(String, nullable=False)
     rating = Column(Integer, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow())
