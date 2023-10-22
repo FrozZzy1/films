ab07539e69f3433da91cdb0f11ef1e5fa38f13a9
FrozZzy1 <frozzzy71@gmail.com>
2023-10-23 01:35:37 +0700

    update(db): update middle_name field

--- db/users.py
+++ db/users.py
@@ -8,7 +8,7 @@ class UserModel(AbstractModel):
     username = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     first_name = Column(String, nullable=False)
-    middle_name = Column(String)
+    middle_name = Column(String, nullable=True)
     last_name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
     is_admin = Column(Boolean, nullable=False, default=False)
